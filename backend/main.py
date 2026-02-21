9fe7712ac1e8b931c348847ca7ca6fd066c370f8
shaynesidman <147111519+shaynesidman@users.noreply.github.com>
2026-02-21 17:58:24 -0500

    change: backend now supports sse instead of websocket because next.js does not integrate with websockets

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI
 import requests
+from sse_starlette import EventSourceResponse
+import json
 from scanner import run_scan 
 
 app = FastAPI()
@@ -9,14 +11,12 @@ def ping():
     response = requests.get("https://api.ipify.org?format=json")
     return response.json()["ip"]
 
-@app.websocket("/scan")
-async def scan(websocket: WebSocket, duration: int = 60, batch_interval: float = 2.0):
-    await websocket.accept()
-    try:
+@app.get("/scan")
+async def scan(duration: int = 60, batch_interval: float = 2.0):
+    async def event_generator():
         async for message in run_scan(duration, batch_interval):
-            await websocket.send_json(message)
-    except WebSocketDisconnect:
-        pass
+            yield { "data": json.dumps(message) }
+    return EventSourceResponse(event_generator())
 
 
 @app.get("/nmap")
